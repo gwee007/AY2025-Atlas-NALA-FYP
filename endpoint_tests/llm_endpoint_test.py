9f0f54f832a0903b92464480ef562943326380ba
yophyoholland <yophyoholland@gmail.com>
2026-01-20 19:38:59 +0800

    updated llm endpoint script by changing gemini model

--- endpoint_tests/llm_endpoint_test.py
+++ endpoint_tests/llm_endpoint_test.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("NALA_API_KEY")
-base_url = os.getenv("BASE_URL")  
+base_url = os.getenv("NALA_BASE_URL")  
 print("API Key:", api_key)
 print("Base URL:", base_url)
 
@@ -13,13 +13,13 @@ url = f"{base_url}/api/llm/"
 
 xml_body = f"""
 <llm_request>
-    <model>gemini-2.5-flash</model>
+    <model>gemini-3-pro-preview</model>
     <system_prompt>You are a precise and technical assistant.</system_prompt>
     <hyperparameters>
         <temperature>0.3</temperature>
         <top_p>0.1</top_p>
     </hyperparameters>
-    <user_prompt>Explain the concept of transfer learning in AI in simple terms.</user_prompt>
+    <user_prompt>Explain what is process control and dynamics.</user_prompt>
 </llm_request>
 """
 
@@ -36,7 +36,7 @@ if response.status_code == 200:
     try:
         # Parse the JSON response
         response_json = response.json()
-        print(response_json)
+        print(response_json['message'])
     except ValueError:
         print("Failed to parse JSON response.")
 else:
